e13a338eaed0b94b9fb7443b1da818f45d175944
Babapool <vitthalsai2001@gmail.com>
2021-06-30 20:27:51 +0530

    added mysql file for insert in user table

---
+++ BackEnd/demo/src/main/resources/insertlogindata.sql
@@ -0,0 +1,3 @@
+Insert into user values (1,b'0',b'0','fBRnT0CD1wNc95KAZJuviA==','Admin','admin@test.com');
+Insert into user values (2,b'0',b'0','nwhWw6U4gNK8DcQleCQ_-g==','projectManager','pmanage@test.com');
+Insert into user values (3,b'0',b'0','28xggY4q5uAXcuZtY1_4zg==','financeUser','fuser@test.com');
